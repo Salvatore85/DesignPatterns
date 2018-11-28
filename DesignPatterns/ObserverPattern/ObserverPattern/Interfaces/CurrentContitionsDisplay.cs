a53d3fd5dceca1731439ad992b3e9d550d55fd14
Salvatore85 <salvatore.m@gmail.com>
2018-11-28 11:14:17 +0100

    Finished the WeatherData class

---
+++ DesignPatterns/ObserverPattern/ObserverPattern/Interfaces/CurrentContitionsDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObserverPattern.Interfaces
+{
+    public class CurrentContitionsDisplay : IObserver, IDisplayElement
+    {
+        public void Display()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void update(float temp, float humidity, float pressure)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- DesignPatterns/ObserverPattern/ObserverPattern/WeatherData.cs
+++ DesignPatterns/ObserverPattern/ObserverPattern/WeatherData.cs
@@ -1,10 +1,53 @@
-﻿using System;
+﻿using ObserverPattern.Interfaces;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ObserverPattern
 {
-    class WeatherData
+    public class WeatherData : ISubject
     {
+        private ArrayList observers;
+        private float temperature;
+        private float humidity;
+        private float pressure;
+
+        public WeatherData()
+        {
+            observers = new ArrayList();
+        }
+
+        public void NotifyObserver()
+        {
+            for (int i = 0; i < observers.Count; i++)
+            {
+                var observer = (IObserver)observers;
+                observer.update(temperature, humidity, pressure);
+            }
+        }
+
+        public void RegisterObserver(IObserver o)
+        {
+            observers.Add(o);
+        }
+
+        public void RemoveObserver(IObserver o)
+        {
+            int i = observers.IndexOf(o);
+            if (i >= 0)
+            {
+                observers.Remove(i);
+            }
+        }
+
+        public void SetMeasurements(float temperature, float humidity, float pressure)
+        {
+            this.temperature = temperature;
+            this.humidity = humidity;
+            this.pressure = pressure;
+
+            MeasurementsChanged();
+        }
     }
 }
