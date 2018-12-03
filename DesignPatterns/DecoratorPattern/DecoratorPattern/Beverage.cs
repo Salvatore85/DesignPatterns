0bfc2361a48d70a1f26027bd1fa0cdba89b0dfb5
Salvatore85 <salvatore.m@gmail.com>
2018-12-03 15:35:19 +0100

    Added the abstract class Beverage

---
+++ DesignPatterns/DecoratorPattern/DecoratorPattern/Beverage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecoratorPattern
+{
+    public abstract class Beverage
+    {
+        string description = "Unnkown Beverage";
+
+        public string Get Description()
+        {
+            return description;
+        }
+
+        public abstract double Cost();
+    }
+
+}
