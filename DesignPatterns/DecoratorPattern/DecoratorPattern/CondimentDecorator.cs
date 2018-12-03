7437d7a1e8603ca43502e485009e300ce3ca2c93
Salvatore85 <salvatore.m@gmail.com>
2018-12-03 16:00:06 +0100

    Added all classes

---
+++ DesignPatterns/DecoratorPattern/DecoratorPattern/CondimentDecorator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecoratorPattern
+{
+    public class CondimentDecorator : Beverage
+    {
+        public override double Cost()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DesignPatterns/DecoratorPattern/DecoratorPattern/HouseBlend.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecoratorPattern
+{
+    public class HouseBlend : Beverage
+    {
+        public override double Cost()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
