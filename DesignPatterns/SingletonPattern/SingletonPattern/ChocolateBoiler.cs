606faf7c1bbf7894f67d93635a37f2f7615dd542
Salvatore85 <salvatore.m@gmail.com>
2018-12-10 13:02:55 +0100

    Updated the README.md

---
+++ DesignPatterns/SingletonPattern/SingletonPattern/ChocolateBoiler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern
+{
+    public class ChocolateBoiler
+    {
+        private static ChocolateBoiler chocolateBoiler;
+
+        private ChocolateBoiler()
+        {
+            Empty = true;
+            Boiled = false;
+        }
+
+        private bool Empty;
+        private bool Boiled;
+
+        public static ChocolateBoiler Instance ()
+        {
+            if(chocolateBoiler == null) 
+            {
+                chocolateBoiler = new ChocolateBoiler();
+            }
+
+            return chocolateBoiler;
+        }
+    }
+}
